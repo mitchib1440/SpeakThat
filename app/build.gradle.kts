72d2b1d5f019b49e2a29c22b07640a30eeeab514
mitchib1440 <oceanstream72@gmail.com>
2025-10-01 23:36:52 +0100

    Upgraded AGP
    About to update my IDE - consider this a respawn point

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.micoyc.speakthat"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.micoyc.speakthat"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 20
         versionName = "1.5.0"
 
