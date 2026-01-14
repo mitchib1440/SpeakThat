e2548a0d85688d3be88ede5956a047c92f6a7358
mitchib1440 <oceanstream72@gmail.com>
2026-01-14 01:22:43 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.micoyc.speakthat"
         minSdk = 24
         targetSdk = 36
-        versionCode = 42
-        versionName = "1.6.4"
+        versionCode = 43
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
