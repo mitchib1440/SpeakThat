20001916d986c2512c26aaef2ee846e0dd33ef03
mitchib1440 <oceanstream72@gmail.com>
2026-09-08 13:11:35 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,10 @@ android {
     defaultConfig {
         applicationId = "com.micoyc.speakthat"
         minSdk = 24
+        // I'm currently sticking with targetSdk 36, as I do not have a modern Android device to test with.
         targetSdk = 36
         versionCode = 61
-        versionName = "1.9.0"
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
